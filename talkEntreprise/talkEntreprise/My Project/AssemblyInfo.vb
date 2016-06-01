c8e5b5e1c5c05ab1d52d54c24b2075e54cedad22
GabRider <gabrielstrano98@gmail.com>
2016-06-01 15:15:09 +0200

    Mise en place du projet

---
+++ talkEntreprise/talkEntreprise/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("talkEntreprise")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("CFPT")> 
+<Assembly: AssemblyProduct("talkEntreprise")> 
+<Assembly: AssemblyCopyright("Copyright © CFPT 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("e3ad71a2-75ea-4e9c-bf01-939bfcddf7b8")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
